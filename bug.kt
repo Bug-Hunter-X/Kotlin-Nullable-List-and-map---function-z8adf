95f0b2c79789d656c672c2f0773bb2ef7ebd9fbb
BugHunter-X <askzenai@gmail.com>
2025-02-01 00:16:06 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val result = list.map { it * 2 }
+    println(result) // Output: [2, 4, 6, 8, 10]
+
+    val emptyList = emptyList<Int>()
+    val emptyResult = emptyList.map { it * 2 }
+    println(emptyResult) // Output: []
+
+    // The bug is here:
+    val nullList: List<Int>? = null
+    val nullResult = nullList?.map { it * 2 }
+    println(nullResult) // Output: null
+
+    //The solution is shown below
+}
